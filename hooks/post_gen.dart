7a378d1040f862503c04e1d50368d2b4515debda
tranvu <trandinhvu1805@gmail.com>
2025-12-07 17:38:45 +0700

    feat(addPackage): add feat

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -1,23 +1,8 @@
 import 'dart:io';
-import 'package:mason/mason.dart';
-
-Future<void> _run(String cmd, List<String> args, HookContext context) async {
-  final result = await Process.run(cmd, args);
-
-  if (result.exitCode != 0) {
-    context.logger.err('Error running: $cmd ${args.join(" ")}');
-    context.logger.err(result.stderr.toString());
-  } else {
-    context.logger.info('✔️ ${cmd} ${args.join(" ")}');
-  }
-}
 
 void run(HookContext context) async {
-  context.logger.info('Adding dependencies...');
+  final progress = context.logger.progress('Installing packages...');
 
-  /// -------------------------------
-  /// Normal Dependencies
-  /// -------------------------------
   final deps = [
     'bloc',
     'equatable',
@@ -29,23 +14,30 @@ void run(HookContext context) async {
     'bloc_lint',
   ];
 
-  /// -------------------------------
-  /// Dev Dependencies
-  /// -------------------------------
   final devDeps = [
     'bloc_test',
     'mocktail',
   ];
 
-  // Add dependencies
   for (final dep in deps) {
     await _run('flutter', ['pub', 'add', dep], context);
   }
 
-  // Add dev dependencies
   for (final dep in devDeps) {
     await _run('flutter', ['pub', 'add', '--dev', dep], context);
   }
 
+  progress.complete();
   context.logger.success('All dependencies installed successfully!');
+}
+
+Future<void> _run(String cmd, List<String> args, HookContext context) async {
+  final result = await Process.run(cmd, args, runInShell: true);
+
+  if (result.exitCode != 0) {
+    context.logger.err('Failed: $cmd ${args.join(" ")}');
+    context.logger.err(result.stderr.toString());
+  } else {
+    context.logger.info('$cmd ${args.join(" ")}');
+  }
 }
